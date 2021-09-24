0463b93f208d0af68a5673d761053b5043f7f3da
sachinbhat2001 <52129700+sachinbhat2001@users.noreply.github.com>
2021-09-24 17:30:29 +0530

    removing some comment

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,7 +6,6 @@ import Person from './Person/Person.js';
 class App extends Component {
   render(){
 
-    //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Jai Shree Ram'));
   
     return (
        <div className="App">
