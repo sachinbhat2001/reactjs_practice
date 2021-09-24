b3fc04a121f555af88593230f69ea836cb6ce78f
sachinbhat2001 <52129700+sachinbhat2001@users.noreply.github.com>
2021-09-24 17:27:57 +0530

    importing Person.js in App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Person from './Person/Person.js';
+class App extends Component {
+  render(){
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Jai Shree Ram</h1>
-    </div>
-  );
-}
+    //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Jai Shree Ram'));
+  
+    return (
+       <div className="App">
+        <h1>Jai Shree Ram</h1>
+        <p> hara hara mahadev</p>
+        <Person></Person>
+      </div>
+     
+      ); 
+  
+//return(a);
+
+        }
+}    
 
 export default App;
